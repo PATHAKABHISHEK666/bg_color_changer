be6b9b4928186de26d34132fc7863f578d743c9a
Abhishek Pathak <144788012+PATHAKABHISHEK666@users.noreply.github.com>
2024-03-03 09:35:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+document.getElementsByTagName("button")[0].style.backgroundColor = "red";
+document.getElementsByTagName("button")[1].style.backgroundColor = "Green";
+document.getElementsByTagName("button")[2].style.backgroundColor = "Blue";
+document.getElementsByTagName("button")[3].style.backgroundColor = "Yellow";
+document.getElementsByTagName("button")[4].style.backgroundColor = "orangered";
+document.getElementsByTagName("button")[5].style.backgroundColor = "#8e06f3";
+document.getElementsByTagName("button")[6].style.backgroundColor = "#58f07b";
+document.getElementsByTagName("button")[7].style.backgroundColor = "#94f2fd";
+document.getElementsByTagName("button")[8].style.backgroundColor = "#8c80ed";
+document.getElementsByTagName("button")[9].style.backgroundColor = "#e432a8";
+document.getElementsByTagName("button")[10].style.backgroundColor = "#e57893";
+document.getElementsByTagName("button")[11].style.backgroundColor = "#ff0709";
+document.getElementsByTagName("button")[12].style.backgroundColor = "#472741";
+document.getElementsByTagName("button")[13].style.backgroundColor = "#d80837";
+document.getElementsByTagName("button")[14].style.backgroundColor = "#d50723";
+document.getElementsByTagName("button")[15].style.backgroundColor = "#a1bee9";
+document.getElementsByTagName("button")[16].style.backgroundColor = "#ca55a3";
+document.getElementsByTagName("button")[17].style.backgroundColor = "#e1cda2";
+document.getElementsByTagName("button")[18].style.backgroundColor = "#fbb248";
+document.getElementsByTagName("button")[19].style.backgroundColor = "#e02e0a";
+
+function c1() {
+  document.body.style.backgroundColor = "red";
+}
+
+function c2() {
+  document.body.style.backgroundColor = "Green";
+}
+function c3() {
+  document.body.style.backgroundColor = "Blue";
+}
+
+function c4() {
+  document.body.style.backgroundColor = "yellow";
+}
+function c5() {
+  document.body.style.backgroundColor = "orangered";
+}
+function c6() {
+  document.body.style.backgroundColor = "#8e06f3";
+}
+function c7() {
+  document.body.style.backgroundColor = "#58f07b";
+}
+function c8() {
+  document.body.style.backgroundColor = "#94f2fd";
+}
+function c9() {
+  document.body.style.backgroundColor = "#8c80ed";
+}
+function c10() {
+  document.body.style.backgroundColor = "#8c80ed";
+}
+function c11() {
+  document.body.style.backgroundColor = "#e57893";
+}
+
+function c12() {
+  document.body.style.backgroundColor = "#ff0709";
+}
+function c13() {
+  document.body.style.backgroundColor = "#472741";
+}
+
+function c14() {
+  document.body.style.backgroundColor = "#d80837";
+}
+function c15() {
+  document.body.style.backgroundColor = "#d50723";
+}
+function c16() {
+  document.body.style.backgroundColor = "#a1bee9";
+}
+function c17() {
+  document.body.style.backgroundColor = "#ca55a3";
+}
+function c18() {
+  document.body.style.backgroundColor = "#e1cda2";
+}
+function c19() {
+  document.body.style.backgroundColor = "#fbb248";
+}
+function c20() {
+  document.body.style.backgroundColor = "#e02e0a";
+}
